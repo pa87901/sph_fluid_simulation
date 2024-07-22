4247c64c8fc239d802c0ce490102e8dd41e40704
Preda Anuvatnujotikul <preda.anuvatnujotikul@yahooinc.com>
2024-07-21 20:12:03 -0700

    Annotating the SPH.cs and SPHCompute.compute shader.

--- Assets/Scripts/SPH.cs
+++ Assets/Scripts/SPH.cs
@@ -5,7 +5,10 @@ using System.Runtime.InteropServices;
 
 [System.Serializable]
 [StructLayout(LayoutKind.Sequential, Size=44)] // So that the particle can be sent to the compute shader.
-// Class for particle object definition. For Navier-Stokes it must be influenced by pressure, density, external force, have a volicity and position vectors.
+// Class for particle object definition.
+// For Navier-Stokes it must be influenced by pressure,
+// density, external force, have a velocity and position
+// vectors.
 public struct Particle {
     public float pressure; // 4
     public float density; // 8
@@ -41,6 +44,7 @@ public class SPH : MonoBehaviour
     public ComputeShader shader;
     public Particle[] particles; // For storing the list of spawned particles to be rendered in the scene.
 
+    // Connect Compute Shader to this SPH script.
     [Header("Fluid Constants")]
     public float boundDamping = -0.3f;
     public float viscosity = -0.003f;
@@ -52,10 +56,11 @@ public class SPH : MonoBehaviour
     // Private variables
     private ComputeBuffer _argsBuffer; // Arguments list for the GPU instance spheres.
     public ComputeBuffer _particlesBuffer; // Contains all the particles in the simulation.
-    private int integrateKernel;
+    private int integrateKernel; // For integrate function to reuse it throughout our script.
     private int computeForceKernel;
     private int densityPressureKernel;
 
+    // Set up all the variables for the Compute Shader.
     private void SetupComputeBuffers() {
         integrateKernel = shader.FindKernel("Integrate");
         computeForceKernel = shader.FindKernel("ComputeForces");
@@ -76,7 +81,7 @@ public class SPH : MonoBehaviour
         shader.SetFloat("radius4", particleRadius * particleRadius * particleRadius * particleRadius);
         shader.SetFloat("radius5", particleRadius * particleRadius * particleRadius * particleRadius * particleRadius);
 
-        shader.SetBuffer(integrateKernel, "_particles", _particlesBuffer);
+        shader.SetBuffer(integrateKernel, "_particles", _particlesBuffer); // Link the particle buffer which needs to be linked per kernel (functions that do the computation).
         shader.SetBuffer(computeForceKernel, "_particles", _particlesBuffer);
         shader.SetBuffer(densityPressureKernel, "_particles", _particlesBuffer);
     }
@@ -84,7 +89,7 @@ public class SPH : MonoBehaviour
     // Native method.
     private void Awake() {
 
-        SpawnParticlesInBox(); // Spawn particles by calling the method to spawn the particles in the grid structure.
+        SpawnParticlesInBox(); // Call the method to spawn the particles in the grid structure.
 
         // Set up arguments for GPU Instanced Particle Rendering.
         uint[] args = {
@@ -95,7 +100,11 @@ public class SPH : MonoBehaviour
             0
         };
 
-        _argsBuffer = new ComputeBuffer(1, args.Length * sizeof(uint), ComputeBufferType.IndirectArguments);
+        _argsBuffer = new ComputeBuffer(
+            1,
+            args.Length * sizeof(uint),
+            ComputeBufferType.IndirectArguments
+        );
         _argsBuffer.SetData(args);
 
         // Setup Particle (Compute) Buffer.
@@ -111,7 +120,7 @@ public class SPH : MonoBehaviour
         shader.SetVector("spherePos", collisionSphere.transform.position);
         shader.SetFloat("sphereRadius", collisionSphere.transform.localScale.x / 2);
 
-        // Total Particles has to be divisible by 100.
+        // Total Particles has to be divisible by 100, because we are using 100 threads in the SPHCompute.compute shader script to ensure the function runs once per particle.
         shader.Dispatch(densityPressureKernel, totalParticles / 100, 1, 1);
         shader.Dispatch(computeForceKernel, totalParticles / 100, 1, 1);
         shader.Dispatch(integrateKernel, totalParticles / 100, 1, 1);
@@ -126,7 +135,11 @@ public class SPH : MonoBehaviour
         for (int x = 0; x < numToSpawn.x; x++) {
             for (int y = 0; y < numToSpawn.y; y++) {
                 for (int z = 0; z < numToSpawn.z; z++) {
-                    Vector3 spawnPos = spawnPoint + new Vector3(x * particleRadius * 2, y * particleRadius * 2, z * particleRadius * 2);
+                    Vector3 spawnPos = spawnPoint + new Vector3(
+                        x * particleRadius * 2,
+                        y * particleRadius * 2,
+                        z * particleRadius * 2
+                    );
                     spawnPos += Random.onUnitSphere * particleRadius * spawnJitter;
                     Particle p  = new Particle {
                         position = spawnPos
@@ -159,7 +172,9 @@ public class SPH : MonoBehaviour
     private void Update() {
         // Render the particles each frame to ensure the shader knows the state of the fluid.
         material.SetFloat(SizeProperty, particleRenderSize);
-        material.SetBuffer(ParticleBufferProperty, _particlesBuffer); // Send the list of particles to the shader, and to update the positions of particles in the compute shader. We therefore do not have ot extract the data from the GPU back to the CPU - V EXPENSIVE.
+        // Send the list of particles to the shader, and to update the positions of particles in the compute shader.
+        // We therefore do not have to extract the data from the GPU back to the CPU - V EXPENSIVE.
+        material.SetBuffer(ParticleBufferProperty, _particlesBuffer);
 
         if (showSpheres) {
             // Unity method to render the instance meshes.
